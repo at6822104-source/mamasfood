43ce7c445f4b617f76f2c793c31fa2c3a28aab9c
at6822104-source <at6822104@gmail.com>
2026-07-01 03:24:28 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,21 +1,3 @@
-// =========================
-// LOADER
-// =========================
-
-window.addEventListener("load", () => {
-
-    const loader = document.getElementById("loader");
-
-    setTimeout(() => {
-        loader.style.opacity = "0";
-
-        setTimeout(() => {
-            loader.style.display = "none";
-        }, 500);
-
-    }, 1500);
-
-});
 
 // =========================
 // AOS
